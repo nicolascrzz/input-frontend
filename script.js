d15590c09d843df35b6bf537c61ce739e537112d
Nicolas <nicolas.crzz14@gmail.com>
2025-12-31 23:42:59 -0300

    Update de segurança e estilização

--- script.js
+++ script.js
@@ -3,19 +3,31 @@ const campo2 = document.getElementById("campo2");
 const campo3 = document.getElementById("campo3");
 const campo4 = document.getElementById("campo4");
 const status = document.getElementById("status");
+const btnSalvar = document.getElementById("btn-salvar");
+const btnAdmin = document.getElementById("btn-admin");
+const modalAdmin = document.getElementById("modal-admin");
+const closeAdmin = document.querySelector(".close-admin");
+const modalData = document.getElementById("modal")
+const abrirModalData = document.getElementById("abriModalData")
+const fecharModalData = document.getElementById("btn-cancelar")
 
 //LOCAL
 const API_URL = "https://input-backend-1.onrender.com";
 
 async function salvar() {
+
+    status.style.opacity = 1;
+    status.style.transition = "";
+
     if (!campo1.value || !campo2.value || !campo4.value) {
         status.innerText = "Preencha os campos obrigatórios!!";
         status.style.color = "red";
         return;
     }
 
-    status.innerText = "Salvando...";
-    status.style.color = "#555";
+    status.innerText = "";
+
+    btnSalvar.classList.add("loading");
 
     await fetch(`${API_URL}/salvar`, {
         method: "POST",
@@ -28,6 +40,8 @@ async function salvar() {
         })
     });
 
+    btnSalvar.classList.remove("loading");
+
     campo1.value = "";
     campo2.value = "";
     campo3.value = "";
@@ -35,6 +49,16 @@ async function salvar() {
 
     status.innerText = "Salvo ✅";
     status.style.color = "#4edaa6ff";
+
+    setTimeout(() => {
+        status.style.transition = "opacity 0.8s";
+        status.style.opacity = 0;
+    }, 2500);
+
+    setTimeout(() => {
+       status.innerText = "";
+       status.style.transition = ""; 
+    }, 3300);
 }
 
 //Enter
@@ -43,14 +67,34 @@ campo4.addEventListener("keydown", e => {
     if (e.key === "Enter") salvar();
 });
 
-
-function abrirModal() {
-    document.getElementById("modal").style.display = "flex";
+abrirModalData.onclick = () => {
+    modalData.style.display = "flex"
 }
 
-function fecharModal() {
-    document.getElementById("modal").style.display = "none";
+fecharModalData.onclick = () => {
+    modalData.style.display = "none"
 }
+
+btnAdmin.onclick = () => {
+    modalAdmin.style.display = "flex";
+}
+
+closeAdmin.onclick = () => {
+    modalAdmin.style.display = "none";
+}
+
+window.addEventListener("click", (event) => {
+    if (event.target === modalAdmin) {
+       modalAdmin.style.display = "none";
+    }
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === modalData) {
+       modalData.style.display = "none";
+    }
+});
+
 
 function exportarExcel() {
     const inicio = document.getElementById("dataInicio").value;
@@ -61,11 +105,29 @@ function exportarExcel() {
         return;
     }
 
-    document.getElementById("btn-exportar").addEventListener("click", () => {
-    const confirmar = confirm("Deseja realmente exportar o relátório Excel?")
+    const confirmar = confirm("Deseja realmente exportar o relátório Excel?");
 
     if (confirmar) {
         window.open(`${API_URL}/exportar-excel`, "_blank");
+    };
+}
+
+async function confirmarAdmin() {
+    
+    const senha = document.getElementById("admin-password").value;
+
+    const resposta = await fetch(`${API_URL}/admin/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ senha })
+    });
+
+    const data = await resposta.json();
+
+    if (data.autorizado) {
+        alert("Acesso Autonizado ✅");
+        closeAdmin();
+    } else {
+        alert("Acesso Negado: Senha incorreta! ❌")
     }
-})
 }
